33655f01e37f513c0f480a78ead0dcad605c03e1
W1zzA <W1zzA@W1ZZK1>
2022-10-01 00:37:00 +0300

    Добавьте файлы проекта.

---
+++ Lab_8/Lab_8.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+/*
+Лабораторная работа №8. Одномерный массив
+12. Дан массив A размера N. Найти максимальный элемент из его элементов с нечетными номерами: A1, A3, A5, ....
+*/
+int main(void)
+{
+	setlocale(LC_ALL, "Russian");
+
+	int  N, i;
+	int a[10];
+
+
+	cout << "Введите N : ";
+	cin >> N;
+	
+	for (i = 0; i < N; ++i) {
+		//cout << i + 1 << endl;
+		printf("a[%i] : ", i + 1);
+		scanf_s("%i", &a[i]);
+		//cin >> a[i];
+	}
+	int max = a[0];
+
+	for (i = 0; i < N; i += 2) {
+		if (a[i] > max) {
+			max = a[i];
+		}
+	}
+
+	cout << max;
+	return 0;
+}
